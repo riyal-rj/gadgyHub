4f6ef3d72b9c2821233ca737445fc21da3db333b
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-01-14 21:00:41 +0530

    backend-complete

--- backend/routes/analytics.routes.js
+++ backend/routes/analytics.routes.js
@@ -2,7 +2,29 @@ import express from 'express';
 import { getAnalytics } from '../controllers/analytics.controller.js';
 import { adminRoute, protectedRoute } from '../middleware/auth.middleware.js';
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/',protectedRoute,adminRoute,getAnalytics);
+router.get('/', protectedRoute, adminRoute, async (req, res) => {
+    try {
+        const analyticsData = await getAnalytics();
+        const endData = new Date();
+
+        const dailySalesData = await getDailySalesData(startDate, endData);
+
+        res.json(
+            {
+                analyticsData,
+                dailySalesData
+            }
+        );
+    } catch (error) {
+        console.log('Error in getAnalytics controller : ' + error.message);
+        return res.status(500).json({
+            status: "failed",
+            message: error.message
+        });
+    }
+
+
+});
 export default router;
