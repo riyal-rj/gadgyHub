0c54585a77a9ca59ca01c1b68e2a897966efb539
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-01-15 02:07:13 +0530

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home.tsx"
+import SignUpPage from "./pages/SignUpPage.tsx"
+import LoginPage from "./pages/LoginPage.tsx"
+import Navbar from "./components/Navbar.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      {/* Background gradient */}
+      <div className='absolute inset-0 overflow-hidden'>
+        <div className='absolute inset-0'>
+          <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full
+      bg-[radial-gradient(ellipse_at_top,rgba(255,215,0,0.3)_0%,rgba(72,61,139,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,77 @@
+import { ShoppingCart, UserPlus2, LogIn, LogOut, Lock,LucideShoppingBag} from 'lucide-react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+    const user: boolean = true;
+    const isAdmin: boolean = false;
+    return (
+        <header className='fixed top-0 left-0 w-full bg-[rgba(72,61,139,0.9)] bg-opacity-90 
+        backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-[rgba(255,215,0,0.7)]'>
+            <div className='flex flex-wrap items-center justify-between px-4 py-2'>
+            <Link to={'/'} className='text-2xl font-bold text-white items-center space-x-2 flex'>
+                <span>e-Kart</span> 
+                <LucideShoppingBag className='text-[rgba(255,215,0,0.9)]' />
+            </Link>
+
+            <nav className='flex flex-wrap items-center gap-4'>
+                <Link to={'/'} className='text-[rgba(255,215,0,0.9)] hover:text-[rgba(72,61,139,0.9)]'>Home</Link>
+                {
+                    user && (
+                        <Link to={'/cart'} className='relative group'>
+                            <ShoppingCart className='text-[rgba(255,215,0,0.9)] hover:text-[rgba(72,61,139,0.9)]' />
+                            <span className='hidden sm:inline text-[rgba(255,215,0,0.9)]'>Cart</span>
+                            <span className='absolute top-0 right-0 w-4 h-4 text-xs text-white bg-[rgba(255,69,58,0.9)] rounded-full flex items-center justify-center'>0</span>
+                        </Link>
+                    )
+                }
+
+                {
+                    isAdmin && (
+                        <Link
+                            className='bg-[rgba(72,61,139,0.9)] hover:bg-[rgba(72,61,139,0.7)] text-[rgba(255,215,0,0.9)] px-3 py-1 rounded-md font-medium 
+             transition duration-300 ease-in-out flex items-center shadow-md'
+                            to={"/admin-dashboard"}
+                        >
+                            <Lock className='inline-block mr-1' size={18} />
+                            <span className='hidden sm:inline'>Dashboard</span>
+                        </Link>
+
+                    )
+                }
+                {
+                    user ? (
+                        <button
+                            className='bg-[rgba(72,61,139,0.9)] hover:bg-[rgba(72,61,139,0.7)] text-[rgba(255,215,0,0.9)] py-2 px-4 rounded- md flex items-center transition duration-300 ease-in-out shadow-md'
+                            onClick={() => { }}
+                        >
+                            <LogOut size={18} />
+                            <span className='hidden sm:inline ml-2'>Log Out</span>
+                        </button>
+                    ) : (
+                        <>
+                            <Link
+                                to={"/signup"}
+                                className='bg-[rgba(255,215,0,0.9)] hover:bg-[rgba(255,215,0,0.7)] text-[rgba(72,61,139,0.9)] py-2 px-4 
+                                rounded-md flex items-center transition duration-300 ease-in-out shadow-md'
+                            >
+                                <UserPlus2 className='mr-2' size={18} />
+                                Sign Up
+                            </Link>
+                            <Link
+                                to={"/login"}
+                                className='bg-[rgba(72,61,139,0.9)] hover:bg-[rgba(72,61,139,0.7)] text-[rgba(255,215,0,0.9)] py-2 px-4 
+                                rounded-md flex items-center transition duration-300 ease-in-out shadow-md'
+                            >
+                                <LogIn className='mr-2' size={18} />
+                                Login
+                            </Link>
+                        </>
+                    )
+                }
+
+            </nav>
+            </div>
+        </header >
+    )
+}
+
+export default Navbar
